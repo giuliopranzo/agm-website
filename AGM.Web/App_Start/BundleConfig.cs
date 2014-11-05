169bbe89b6522a5879b21a255ed4c87220470f83
Fernando Walter Gagni <nandowalter@gmail.com>
2014-11-05 20:56:41 +0100

    Iniziale

---
+++ AGM.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+
+namespace AGM.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app/common.js",
+                "~/Scripts/app/_run.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/Content/bootstrap.css",
+                 "~/Content/Site.css"));
+
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
