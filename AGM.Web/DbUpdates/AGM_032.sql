1487d06dc7c1e0b98a29a673ff32bf31b6cb78eb
Giulio <giulio.pranzo@gmail.com>
2016-11-14 09:31:38 +0100

    Fix on Users isShiftWorker column

--- AGM.Web/DbUpdates/AGM_032.sql
+++ AGM.Web/DbUpdates/AGM_032.sql
@@ -10,11 +10,11 @@ GO
 IF NOT EXISTS(
     SELECT *
     FROM sys.columns 
-    WHERE Name      = N'IsShiftWorker'
+    WHERE Name      = N'isShiftWorker'
       AND Object_ID = Object_ID(N'utenti'))
 BEGIN
-    ALTER TABLE utenti ADD IsShiftWorker BIT NULL;
-	EXEC('UPDATE utenti SET utenti.IsShiftWorker = 0');
+    ALTER TABLE utenti ADD isShiftWorker INT NULL;
+	EXEC('UPDATE utenti SET utenti.isShiftWorker = 0');
 END
 
 GO
